c28417c4d010f5b35030f7d17d227233b7b78b1d
Neil Parsons <neilparsons@gmail.com>
2021-01-08 16:29:44 -0400

    Support calling from command line

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 mod cpu;
 mod mmu;
 mod window;
+
+pub fn run(frequency: u32, file_path: &str) {
+    print!("{:?}", frequency);
+    print!("{:?}", file_path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+use clap::{Arg, App, value_t_or_exit};
+use chip8;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("chip8")
+        .arg(
+            Arg::with_name("FREQUENCY")
+                .short("f")
+                .long("freq")
+                .takes_value(true)
+                .default_value("500")
+                .help("Sets the CPU frequency in hz"),
+        )
+        .arg(Arg::with_name("FILE")
+            .help("The ch8 binary file to load")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+    let frequency = value_t_or_exit!(matches.value_of("FREQUENCY"), u32);
+    let file_path = matches.value_of("FILE").unwrap();
+
+    chip8::run(frequency, file_path);
 }
